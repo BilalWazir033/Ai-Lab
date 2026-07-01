f163c831f072c52ff7bee37820e87fe336caef0f
BilalWazir033 <bilalwaziri2006@gmail.com>
2026-07-01 22:28:20 +0500

    .

---
+++ Ai Course by CampusX/word.py
@@ -0,0 +1,16 @@
+print("My world")
+for i in range(2):
+    print("Hello")
+    print("World")
+    print("My world")
+    print("You are mine")
+    print("We are with you")
+
+
+
+
+
+
+    git add .
+    git commit -m "."
+    git push origin main
